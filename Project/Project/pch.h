0e170d6ba9e2d9f0290d46810b84a11e0239ed22
Choheeseok <dswill038@gmail.com>
2021-03-04 11:46:51 +0900

    윈도우 프로그램 생성하기

---
+++ Project/Project/pch.h
@@ -0,0 +1,14 @@
+﻿#ifndef PCH_H
+#define PCH_H
+
+#include "targetver.h"
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
+#endif //PCH_H
