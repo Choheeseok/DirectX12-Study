9fec2f61cc1750055b44395e9c5f7337956a1eb1
Choheeseok <dswill038@gmail.com>
2021-03-05 01:27:51 +0900

    게임 프로그램의 골격 작성하기

--- Project/Project/pch.h
+++ Project/Project/pch.h
@@ -11,4 +11,28 @@
 #include <memory.h>
 #include <tchar.h>
 
-#endif //PCH_H
+#include <string>
+#include <wrl.h>
+#include <shellapi.h>
+
+#include <d3d12.h>
+#include <dxgi1_4.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+#include <DirectXPackedVector.h>
+#include <DirectXColors.h>
+#include <DirectXCollision.h>
+
+using namespace DirectX;
+using namespace DirectX::PackedVector;
+
+using Microsoft::WRL::ComPtr;
+
+#pragma comment(lib, "d3dcompiler.lib")
+#pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "dxgi.lib")
+
+#define FRAME_BUFFER_WIDTH  640
+#define FRAME_BUFFER_HEIGHT 480
+
+#endif
