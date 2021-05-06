28265c655c9155af61a3262654ac9a1bf65faad6
Choheeseok <dswill038@gmail.com>
2021-05-07 04:53:18 +0900

    보간된 삼각형 그리기

--- Project/Project/Scene.h
+++ Project/Project/Scene.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Timer.h"
+#include "Shader.h"
 
 class Scene
 {
@@ -10,15 +11,24 @@ public:
 	bool OnProcessingMouseMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM lParam);
 	bool OnProcessingKeyboardMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM lParam);
 
-	void BuildObjects(ID3D12Device* pd3dDevice);
+	void BuildObjects(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList);
 	void ReleaseObjects();
 
 	bool ProcessInput();
 	void AnimateObjects(float fTimeElapsed);
 	void Render(ID3D12GraphicsCommandList* pd3dCommandList);
 
+	void ReleaseUploadBuffers();
+
+	// 그래픽 루트 시그너처를 생성한다.
+	ID3D12RootSignature* CreateGraphicsRootSignature(ID3D12Device* pd3dDevice);
+	ID3D12RootSignature* GetGraphicsRootSignature();
+
+protected:
+	// 씬은 셰이더들의 집합이다.
+	// 셰이더들은 게임 객체들의 집합이다.
+	Shader** m_ppShaders = NULL;
+	int m_nShaders = 0;
+
 	ID3D12RootSignature* m_pd3dGraphicsRootSignature;
-	// 루트 시그너처를 나타내는 인터페이스 포인터이다.
-	ID3D12PipelineState* m_pd3dPipelineState;
-	// 파이프라인 상태를 나타내는 인터페이스 포인터이다.
 };
